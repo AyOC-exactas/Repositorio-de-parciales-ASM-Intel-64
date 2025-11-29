8fe6d360ff66da93bafa90e4489e0745cf6bc9ef
Agustín Alejo Fernández Ortuzar <33966396+afernandezortuzar@users.noreply.github.com>
2025-11-29 13:38:57 -0300

    Update tester.sh to handle timeouts and results
    Refactor test script to include timeout and pass criteria.

--- P1-ComFurfi-2c2025/tester.sh
+++ P1-ComFurfi-2c2025/tester.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
-(cd src/ej1 && make valgrind_abi && echo -e '\033[0;32mTests EJ1 corrieron exitosamente\033[0m')
+(timeout 30s bash -c 'cd src/ej1 && make clean && make valgrind_abi && echo -e "\033[0;32mTests EJ1 corrieron exitosamente\033[0m"')
 EJ1_STATUS=$?
 
-(cd src/ej2 && make valgrind_abi && echo -e '\033[0;32mTests EJ2 corrieron exitosamente\033[0m')
+(timeout 30s bash -c 'cd src/ej2 && make clean && make valgrind_abi && echo -e "\033[0;32mTests EJ2 corrieron exitosamente\033[0m"')
 EJ2_STATUS=$?
 
-(cd src/ej3 && make valgrind_abi && echo -e '\033[0;32mTests EJ3 corrieron exitosamente\033[0m')
+(timeout 30s bash -c 'cd src/ej3 && make clean && make valgrind_abi && echo -e "\033[0;32mTests EJ3 corrieron exitosamente\033[0m"')
 EJ3_STATUS=$?
 
-# If either test failed, exit with non-zero
-if [ $EJ1_STATUS -ne 0 ] || [ $EJ2_STATUS -ne 0 ] || [ $EJ3_STATUS -ne 0 ]; then
-  echo
-  echo -e '\033[0;31mAl menos uno de los tests falló\033[0m'
-  exit 1
+ejercicios_correctos=0
+
+if [ "$EJ1_STATUS" -eq 0 ]; then
+    ((ejercicios_correctos++))
+fi
+
+if [ "$EJ2_STATUS" -eq 0 ]; then
+    ((ejercicios_correctos++))
+fi
+
+if [ "$EJ3_STATUS" -eq 0 ]; then
+    ((ejercicios_correctos++))
+fi
+
+if [ "$ejercicios_correctos" -le 1 ]; then
+    echo
+    echo -e '\033[0;31mDesaprobado: Hay errores en más de un ejercicio\033[0m'
+    exit 1
 else
-  echo
-  echo -e '\033[0;32mTodos los tests corrieron exitosamente\033[0m'
-  exit 0
+    echo
+    echo -e '\033[0;32mAprobado: Al menos dos ejercicios no tienen errores\033[0m'
+    exit 0
 fi
